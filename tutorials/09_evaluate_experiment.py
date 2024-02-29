dbbc9e93b33c826cfcd1afcde10489bffae37a7c
Jiaying Cheng <jcheng@cluster.ies.uni-kassel.de>
2024-02-29 20:02:06 +0100

    changing visual

--- tutorials/09_evaluate_experiment.py
+++ tutorials/09_evaluate_experiment.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
         plt.errorbar(np.arange(16, (len(result_mean)+1)*16, 16), result_mean, result_std,
                     label=f"({np.mean(result_mean):.4f}) {qs_name}", alpha=0.3, color=color)
         
-    plt.legend(bbox_to_anchor =(0.5,-0.4), loc='lower center', ncol=2)
+    plt.legend(bbox_to_anchor =(0.5,-0.25), loc='lower center', ncol=3)
     plt.tight_layout()
     plt.xlabel('# Labels queried')
     if graph_type == "time":
@@ -67,8 +67,9 @@ if __name__ == "__main__":
     else:
         plt.ylabel("Accuracy")
     # output_path = f'{dataset_name}_{graph_type}.pdf'
+    plt.title(dataset_name)
     output_path = f'/mnt/stud/home/jcheng/scikit-activeml/tutorials/result_param/{dataset_name}_{graph_type}.pdf'
-    plt.savefig(output_path)
+    plt.savefig(output_path, bbox_inches="tight")
 
 
 
