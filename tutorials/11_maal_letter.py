d004d8767389bce8f90fb58c1bac8bd23fcb6cf6
Cheng-JY <chengjiaying.prc@gmail.com>
2024-06-09 18:40:37 +0200

    add train for dataset letter

---
+++ tutorials/11_maal_letter.py
@@ -0,0 +1,189 @@
+import matplotlib as mlp
+import matplotlib.pyplot as plt
+import numpy as np
+
+from copy import deepcopy
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+from skactiveml.classifier import SkorchClassifier
+from skactiveml.classifier.multiannotator import CrowdLayerClassifier
+from skactiveml.pool import RandomSampling
+from skactiveml.pool.multiannotator import SingleAnnotatorWrapper
+from skactiveml.utils import majority_vote, is_labeled, is_unlabeled
+from skorch.callbacks import LRScheduler
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+from tqdm import tqdm
+
+
+class ClassifierModule(nn.Module):
+    def __init__(self, n_classes, n_features, dropout):
+        super(ClassifierModule, self).__init__()
+        n_hidden_neurons_1 = 256
+        n_hidden_neurons_2 = 128
+        self.embed_X_block = nn.Sequential(
+            nn.Linear(in_features=n_features, out_features=n_hidden_neurons_1),
+            nn.BatchNorm1d(num_features=n_hidden_neurons_1),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.Linear(in_features=n_hidden_neurons_1, out_features=n_hidden_neurons_2),
+            nn.BatchNorm1d(num_features=n_hidden_neurons_2),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+        )
+        self.mlp = nn.Linear(in_features=n_hidden_neurons_2, out_features=n_classes)
+
+    def forward(self, x):
+        embed_x = self.embed_X_block(x)
+        logit_class = self.mlp(embed_x)
+
+        return logit_class
+
+
+def load_dataset_letter(random_state=42):
+    data_dir = 'dataset/letter'
+    X = np.load(f'{data_dir}/letter-X.npy').astype(np.float32)
+    y = np.load(f'{data_dir}/letter-y.npy')
+    y_true = np.load(f'{data_dir}/letter-y-true.npy')
+
+    X_train, X_test, y_train, y_test, y_train_true, y_test_true = train_test_split(X, y, y_true, test_size=0.2,
+                                                                                   random_state=random_state)
+    sc = StandardScaler().fit(X_train)
+    X_train = sc.transform(X_train)
+    X_test = sc.transform(X_test)
+    return X_train, X_test, y_train, y_test, y_train_true, y_test_true
+
+
+def seed_everything(seed=42):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+
+if __name__ == '__main__':
+    MISSING_LABEL = -1
+    RANDOM_STATE = 0
+
+    seed_everything(RANDOM_STATE)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # load dataset
+    X_train, X_test, y_train, y_test, y_train_true, y_test_true = load_dataset_letter(RANDOM_STATE)
+
+    classes = np.unique(y_train_true)
+    n_classes = len(classes)
+    n_features = X_train.shape[1]
+    n_annotators = y_train.shape[1]
+    n_samples = X_train.shape[0]
+
+    # performance
+    misclassification = []
+
+    # Generate model according to config
+    hyper_parameter = {
+        'max_epochs': 50,
+        'batch_size': 64,
+        'lr': 0.01,
+        'optimizer__weight_decay': 0.0,
+    }
+    lr_scheduler = LRScheduler(policy='CosineAnnealingLR', T_max=hyper_parameter['max_epochs'])
+
+    # randomly add missing labels
+    y_partial = np.full_like(y_train, fill_value=MISSING_LABEL)
+    initial_label_size = 100
+
+    for a_idx in range(n_annotators):
+        random_state = np.random.RandomState(a_idx)
+        is_lbld_a = is_labeled(y_train[:,a_idx], missing_label=MISSING_LABEL)
+        p_a = is_lbld_a / is_lbld_a.sum()
+        initial_label_size = min(initial_label_size, is_lbld_a.sum())
+        selected_idx_a = random_state.choice(np.arange(n_samples), size=initial_label_size, p=p_a, replace=False)
+        y_partial[selected_idx_a, a_idx] = y_train[selected_idx_a, a_idx]
+
+    # Create query strategy
+    sa_qs = RandomSampling(random_state=RANDOM_STATE, missing_label=MISSING_LABEL)
+    ma_qs = SingleAnnotatorWrapper(sa_qs, random_state=RANDOM_STATE, missing_label=MISSING_LABEL)
+    candidate_indices = np.arange(n_samples)
+
+    # Function to be able to index via an array of indices
+    idx = lambda A: (A[:, 0], A[:, 1])
+
+    # Fallback for random annotator selection
+    A_random = np.ones_like(y_partial)
+
+    n_al_cycle = 25
+    al_batch_size = 128
+    nn_name = 'cl-performance'
+
+    for c in range(n_al_cycle+1):
+        if c > 0:
+            # set the performance
+            if nn_name in ['mv', 'cl-random']:
+                A_perf = A_random
+            else:
+                A_perf = net.predict_annotator_perf(X_train)
+            y_query = np.copy(y_partial)
+            is_ulbld_query = np.copy(is_ulbld)
+            is_candidate = is_ulbld_query.all(axis=-1)
+            candidates = candidate_indices[is_candidate]
+            query_indices = ma_qs.query(
+                X=X_train,
+                y=y_partial,
+                candidates=candidates,
+                A_perf=A_perf[candidates],
+                batch_size=al_batch_size,
+                n_annotators_per_sample=1,
+            )
+            y_partial[idx(query_indices)] = y_train[idx(query_indices)]
+
+        if nn_name in ['mv']:
+            # Generate model
+            net = SkorchClassifier(
+                    ClassifierModule,
+                    module__n_classes=n_classes,
+                    module__n_features=n_features,
+                    module__dropout=0.5,
+                    classes=classes,
+                    missing_label=MISSING_LABEL,
+                    cost_matrix=None,
+                    random_state=RANDOM_STATE+c,
+                    criterion=nn.CrossEntropyLoss(),
+                    train_split=None,
+                    verbose=False,
+                    optimizer=torch.optim.RAdam,
+                    device=device,
+                    callbacks=[lr_scheduler],
+                    **hyper_parameter
+                )
+            y_agg = majority_vote(y_partial, classes=classes, missing_label=MISSING_LABEL, random_state=RANDOM_STATE + c)
+            net.fit(X_train, y_agg)
+        else:
+            gt_net = ClassifierModule(n_classes=n_classes, n_features=n_features, dropout=0.5)
+            net = CrowdLayerClassifier(
+                module__n_classes=n_classes,
+                module__n_annotators=n_annotators,
+                module__gt_net=gt_net,
+                classes=classes,
+                missing_label=MISSING_LABEL,
+                cost_matrix=None,
+                random_state=RANDOM_STATE+c,
+                train_split=None,
+                verbose=False,
+                optimizer=torch.optim.RAdam,
+                device=device,
+                callbacks=[lr_scheduler],
+                **hyper_parameter,
+            )
+            net.fit(X_train, y_partial)
+
+        accuracy = net.score(X_test, y_test_true)
+        misclassification.append(1-accuracy)
+        print(c, accuracy)
+        is_ulbld = is_unlabeled(y_partial, missing_label=MISSING_LABEL)
+
+    plt.plot(misclassification)
+    plt.title(f'{nn_name} + letter + majority-voting + random-sampling')
+    plt.show()
---
+++ tutorials/13_train_letter.py
@@ -0,0 +1,170 @@
+import os
+import sys
+import time
+
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from skorch.callbacks import LRScheduler
+
+from skactiveml.utils import majority_vote
+
+sys.path.append('..')
+
+import numpy as np
+import pandas as pd
+
+import torch
+from torch import nn
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from skactiveml.classifier import SkorchClassifier
+from skactiveml.classifier.multiannotator import CrowdLayerClassifier
+
+import mlflow
+
+
+def load_dataset_letter(random_state=42):
+    data_dir = 'dataset/letter'
+    X = np.load(f'{data_dir}/letter-X.npy')
+    y = np.load(f'{data_dir}/letter-y.npy')
+    y_true = np.load(f'{data_dir}/letter-y-true.npy')
+
+    X_train, X_test, y_train, y_test, y_train_true, y_test_true = train_test_split(X, y, y_true, test_size=0.2, random_state=random_state)
+    sc = StandardScaler().fit(X_train)
+    X_train = sc.transform(X_train)
+    X_test = sc.transform(X_test)
+    return X_train, X_test, y_train, y_test, y_train_true, y_test_true
+
+
+def seed_everything(seed=42):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+
+class ClassifierModule(nn.Module):
+    def __init__(self, n_classes, n_features, dropout):
+        super(ClassifierModule, self).__init__()
+        n_hidden_neurons_1 = 256
+        n_hidden_neurons_2 = 128
+        self.embed_X_block = nn.Sequential(
+            nn.Linear(in_features=n_features, out_features=n_hidden_neurons_1),
+            nn.BatchNorm1d(num_features=n_hidden_neurons_1),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.Linear(in_features=n_hidden_neurons_1, out_features=n_hidden_neurons_2),
+            nn.BatchNorm1d(num_features=n_hidden_neurons_2),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+        )
+        self.mlp = nn.Linear(in_features=n_hidden_neurons_2, out_features=n_classes)
+
+    def forward(self, x):
+        embed_x = self.embed_X_block(x)
+        logit_class = self.mlp(embed_x)
+
+        return logit_class
+
+
+if __name__ == '__main__':
+    seed = 0
+    MISSING_LABEL = -1
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    seed_everything(seed)
+    X_train, X_test, y_train, y_test, y_train_true, y_test_true = load_dataset_letter(seed)
+
+    dataset_classes = np.unique(y_test_true)
+    n_classes = len(dataset_classes)
+    n_features = X_train.shape[1]
+    n_annotators = y_train.shape[1]
+
+    mlflow.set_tracking_uri(uri='/Users/chengjiaying/PycharmProjects/scikit-activeml/tutorials/tracking')
+    exp = mlflow.get_experiment_by_name(name='Letter-Training')
+    experiment_id = mlflow.create_experiment(name='Letter-Training') if exp is None else exp.experiment_id
+
+    with mlflow.start_run(experiment_id=experiment_id) as active_run:
+        hyper_dict = {
+            'max_epochs': 50,
+            'batch_size': 64,
+            'lr': 0.01,
+            'optimizer__weight_decay': 0.0,
+        }
+        lr_scheduler = LRScheduler(policy="CosineAnnealingLR", T_max=hyper_dict['max_epochs'])
+
+        nn_name = 'lb'
+        if nn_name == 'cl':
+            gt_net = ClassifierModule(n_classes=n_classes, n_features=n_features, dropout=0.5)
+            net = CrowdLayerClassifier(
+                module__n_classes=n_classes,
+                module__n_annotators=n_annotators,
+                module__gt_net=gt_net,
+                classes=dataset_classes,
+                missing_label=MISSING_LABEL,
+                cost_matrix=None,
+                random_state=seed,
+                train_split=None,
+                verbose=False,
+                optimizer=torch.optim.RAdam,
+                device=device,
+                callbacks=[lr_scheduler],
+                **hyper_dict
+            )
+        elif nn_name == 'ub' or nn_name == 'lb':
+            net = SkorchClassifier(
+                ClassifierModule,
+                module__n_classes=n_classes,
+                module__n_features=n_features,
+                module__dropout=0.5,
+                classes=dataset_classes,
+                missing_label=MISSING_LABEL,
+                cost_matrix=None,
+                random_state=1,
+                criterion=nn.CrossEntropyLoss(),
+                train_split=None,
+                verbose=False,
+                optimizer=torch.optim.RAdam,
+                device=device,
+                callbacks=[lr_scheduler],
+                **hyper_dict
+            )
+            if nn_name == 'ub':
+                y_train = y_train_true
+            elif nn_name == 'lb':
+                y_train = majority_vote(y_train, classes=dataset_classes, missing_label=-1)
+                print(accuracy_score(y_train, y_train_true))
+
+        hyper_dict['nn_name'] = nn_name
+        hyper_dict['seed'] = seed
+        mlflow.log_params(hyper_dict)
+
+        start = time.time()
+        net.fit(X_train, y_train)
+        end = time.time()
+
+        train_accuracy = net.score(X_train, y_train_true)
+
+        p_pred = net.predict_proba(X_test)
+        print(p_pred[0])
+        test_accuracy = net.score(X_test, y_test_true)
+        metrics = {
+            'train_accuracy': train_accuracy,
+            'test_accuracy': test_accuracy,
+            'time': end-start,
+        }
+        mlflow.log_metrics(metrics)
+        print(metrics)
+
+        history = net.history
+        train_loss = history[:, 'train_loss']
+
+        plt.plot(train_loss)
+        plt.title(f'lr: {hyper_dict["lr"]}; weight decay: {hyper_dict["optimizer__weight_decay"]}\n'
+                  f'train: {metrics["train_accuracy"]}; test: {metrics["test_accuracy"]}')
+        plt.show()
+
+        loss = {'train_loss': train_loss}
+        df = pd.DataFrame.from_dict(data=loss)
+        outpath = active_run.info.artifact_uri
+        outpath = os.path.join(outpath, "result.csv")
+        df.to_csv(outpath, index=False)
