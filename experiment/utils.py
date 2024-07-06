aca6ada2cc7bf0587110d7b35f1fd1c5b7a4565e
Jiaying Cheng <jcheng@cluster.ies.uni-kassel.de>
2024-07-06 14:37:29 +0200

    remove unused code

--- experiment/utils.py
+++ experiment/utils.py
@@ -10,23 +10,6 @@ from sklearn.datasets import fetch_openml
 import torch
 
 
-def get_experiment_result(uri, experiment_name, path, train_name):
-    mlflow.set_tracking_uri(uri=uri)
-
-    experiment = mlflow.get_experiment_by_name(experiment_name)
-    df = mlflow.search_runs(experiment_ids=experiment.experiment_id, output_format="pandas")
-
-    if train_name == 'cl':
-        csv_df = df[['params.model_name', 'params.batch_size', 'params.max_epochs', 'params.lr',
-                 'params.optimizer__weight_decay', 'metrics.train_accuracy', 'metrics.test_accuracy', 'metrics.time']]
-    elif train_name == 'label-me':
-        print(df.columns)
-        csv_df = df[['params.nn_name', 'params.batch_size', 'params.max_epochs', 'params.lr', 'params.seed',
-                     'params.optimizer__weight_decay', 'metrics.train_accuracy', 'metrics.test_accuracy',
-                     'metrics.time']]
-    csv_df.to_csv(path, index=False)
-
-
 def load_dataset(name, data_dir,random_state=42):
     if name == 'letter':
         return load_dataset_letter(data_dir)
@@ -44,12 +27,11 @@ def load_dataset_letter(data_dir, random_state=42):
     X_test = sc.transform(X_test)
     return X_train, X_test, y_train, y_test, y_train_true, y_test_true
 
-def load_dataset_letter_2(random_state=42):
-    data_dir = 'dataset/letter'
+def load_dataset_letter_2(data_dir, random_state=42):
     X, y_true = fetch_openml(data_id=6, cache=True, return_X_y=True)
     X = X.values.astype(np.float32)
     y_true = LabelEncoder().fit_transform(y_true.values)
-    y_new = torch.load(f'{data_dir}/letter-annot-mix.pt').numpy()
+    y_new = torch.load(f'{data_dir}/letter/letter-annot-mix.pt').numpy()
 
     train, test = train_test_split(
         np.arange(len(y_true)), test_size=0.2, random_state=0, stratify=y_true
@@ -64,12 +46,3 @@ def load_dataset_letter_2(random_state=42):
     X_train = sc.transform(X_train)
     X_test = sc.transform(X_test)
     return X_train, X_test, y_train, y_test, y_train_true, y_test_true
-
-
-if __name__ == '__main__':
-    uri_training = '/Users/chengjiaying/PycharmProjects/scikit-activeml/tutorials/tracking'
-    # Label-Me-Training
-    # Crowd-Layer-Training
-    exp_training = 'Letter-Training'
-    path_training = 'training_letter.csv'
-    get_experiment_result(uri_training, exp_training, path_training, 'label-me')
