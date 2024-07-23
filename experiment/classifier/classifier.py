63fae999baa723c1081786b883660ee2eff58ef2
Cheng-JY <chengjiaying.prc@gmail.com>
2024-07-23 15:43:33 +0200

    add fit classifier

--- experiment/classifier/classifier.py
+++ experiment/classifier/classifier.py
@@ -24,8 +24,9 @@ class TabularClassifierModule(nn.Module):
 
         return logit_class
 
+
 class TabularClassifierGetEmbedXModule(nn.Module):
-    def __init__(self, n_classes, n_features, dropout):
+    def __init__(self, n_features, dropout):
         super(TabularClassifierGetEmbedXModule, self).__init__()
         n_hidden_neurons_1 = 256
         n_hidden_neurons_2 = 128
@@ -39,34 +40,20 @@ class TabularClassifierGetEmbedXModule(nn.Module):
             nn.ReLU(),
             nn.Dropout(p=dropout),
         )
-        self.mlp = nn.Linear(in_features=n_hidden_neurons_2, out_features=n_classes)
 
     def forward(self, x):
         embed_x = self.embed_X_block(x)
-        logit_class = self.mlp(embed_x)
 
-        return logit_class
+        return embed_x
 
 
 class TabularClassifierGetOutputModule(nn.Module):
-    def __init__(self, n_classes, n_features, dropout):
+    def __init__(self, n_classes):
         super(TabularClassifierGetOutputModule, self).__init__()
-        n_hidden_neurons_1 = 256
         n_hidden_neurons_2 = 128
-        self.embed_X_block = nn.Sequential(
-            nn.Linear(in_features=n_features, out_features=n_hidden_neurons_1),
-            nn.BatchNorm1d(num_features=n_hidden_neurons_1),
-            nn.ReLU(),
-            nn.Dropout(p=dropout),
-            nn.Linear(in_features=n_hidden_neurons_1, out_features=n_hidden_neurons_2),
-            nn.BatchNorm1d(num_features=n_hidden_neurons_2),
-            nn.ReLU(),
-            nn.Dropout(p=dropout),
-        )
         self.mlp = nn.Linear(in_features=n_hidden_neurons_2, out_features=n_classes)
 
     def forward(self, x):
-        embed_x = self.embed_X_block(x)
-        logit_class = self.mlp(embed_x)
+        logit_class = self.mlp(x)
 
         return logit_class
