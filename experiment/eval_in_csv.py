ad4f6eb2816ee01b18f80bc49801bc9d9e519750
Cheng-JY <chengjiaying.prc@gmail.com>
2024-08-02 12:52:05 +0200

    add batch_size

--- experiment/eval_in_csv.py
+++ experiment/eval_in_csv.py
@@ -39,39 +39,43 @@ def main(cfg):
     annotator_query_strategies = df['params.annotator_query_strategy'].unique()
     learning_strategies = df['params.learning_strategy'].unique()
     n_annotators_per_sample_list = df['params.n_annotators_per_sample'].unique()
+    batch_size_list = df['params.batch_size'].unique()
 
     for iqs_name in instance_query_strategies:
         for aqs_name in annotator_query_strategies:
             for ls_name in learning_strategies:
                 for n_per_sample in n_annotators_per_sample_list:
+                    for batch_size in batch_size_list:
 
-                    df_qs = df.loc[(df['params.instance_query_strategy'] == iqs_name) &
-                                   (df['params.annotator_query_strategy'] == aqs_name) &
-                                   (df['params.learning_strategy'] == ls_name) &
-                                   (df['params.n_annotators_per_sample'] == n_per_sample)]
-                    r = []
-                    for idx, row in df_qs.iterrows():
-                        artifact = os.path.join(row.artifact_uri, 'result.csv')
-                        # artifact = artifact.split("file://")[1]
-                        print(artifact)
-                        print(os.path.exists(artifact))
-                        if os.path.exists(artifact):
-                            result_qs = pd.read_csv(artifact, index_col=None)
-                            r.append(result_qs)
+                        df_qs = df.loc[(df['params.instance_query_strategy'] == iqs_name) &
+                                       (df['params.annotator_query_strategy'] == aqs_name) &
+                                       (df['params.learning_strategy'] == ls_name) &
+                                       (df['params.n_annotators_per_sample'] == n_per_sample) &
+                                       (df['params.batch_size'] == batch_size)]
+                        r = []
+                        for idx, row in df_qs.iterrows():
+                            artifact = os.path.join(row.artifact_uri, 'result.csv')
+                            # artifact = artifact.split("file://")[1]
+                            print(artifact)
+                            print(os.path.exists(artifact))
+                            if os.path.exists(artifact):
+                                result_qs = pd.read_csv(artifact, index_col=None)
+                                r.append(result_qs)
 
-                    if len(r) != 0:
-                        results = pd.concat(r)
-                        mean_and_std = []
-                        for column in results.columns:
-                            result = results.groupby(['step'])[column].agg(['mean', 'std']).set_axis([f'{column}_mean', f'{column}_std'], axis=1)
-                            mean_and_std.append(result)
-                        r_mean_and_std = pd.concat(mean_and_std, axis=1)
-                        label = (f'{iqs_name} '
-                                 f'+ {aqs_name} '
-                                 f'+ {ls_name} '
-                                 f'+ {n_per_sample}')
-                        output_path = f'{cfg["output_file_path"][running_device]}/result_letter/{label}.csv'
-                        r_mean_and_std.to_csv(output_path, index=False)
+                        if len(r) != 0:
+                            results = pd.concat(r)
+                            mean_and_std = []
+                            for column in results.columns:
+                                result = results.groupby(['step'])[column].agg(['mean', 'std']).set_axis([f'{column}_mean', f'{column}_std'], axis=1)
+                                mean_and_std.append(result)
+                            r_mean_and_std = pd.concat(mean_and_std, axis=1)
+                            label = (f'{iqs_name} '
+                                     f'+ {aqs_name} '
+                                     f'+ {ls_name} '
+                                     f'+ {n_per_sample}'
+                                     f'+ {batch_size}')
+                            output_path = f'{cfg["output_file_path"][running_device]}/result_letter/{label}.csv'
+                            r_mean_and_std.to_csv(output_path, index=False)
 
 
 if __name__ == "__main__":
--- experiment/plot_ipywidget.py
+++ experiment/plot_ipywidget.py
@@ -4,52 +4,34 @@ import pandas as pd
 import ipywidgets as widgets
 from IPython.core.display_functions import display
 
-metrics = ['misclassification', 'error_annotation_rate']
-for i in range(10):
-    metrics.append(f"Number_of_annotations_{i}")
-    metrics.append(f"Number_of_correct_annotation_{i}")
-
 
 def plot_graph(
-        is_random=True,
-        is_uncertainty=False,
-        is_core_set=False,
-        is_n_anno_1=False,
-        is_n_anno_2=True,
-        metric='Number_of_annotations_1',
+        instance_query_strategies,
+        annotator_query_strategies,
+        learning_strategies,
+        n_annotator_list,
+        metric,
+        name,
 ):
     output_path = '/Users/chengjiaying/PycharmProjects/scikit-activeml/experiment/output_image/'
 
-    instance_query_strategies = []
-    n_annotator_per_instance_list = []
-    if is_random:
-        instance_query_strategies.append('random')
-    if is_uncertainty:
-        instance_query_strategies.append('uncertainty')
-    if is_core_set:
-        instance_query_strategies.append('coreset')
-
-    if is_n_anno_1:
-        n_annotator_per_instance_list.append('1')
-    if is_n_anno_2:
-        n_annotator_per_instance_list.append('2')
-
-    annotator_query_strategies = ['random', 'round-robin', 'trace-reg', 'geo-reg-f', 'geo-reg-w']
-    learning_strategies = ['majority-vote', 'trace-reg', 'geo-reg-f', 'geo-reg-w']
     batch_size = 256
 
     for instance_query_strategy in instance_query_strategies:
         for annotator_query_strategy in annotator_query_strategies:
             for learning_strategy in learning_strategies:
-                for n_annotator_per_instance in n_annotator_per_instance_list:
+                for n_annotator_per_instance in n_annotator_list:
                     if (annotator_query_strategy in ['trace-reg', 'geo-reg-f', 'geo-reg-w'] and
                             learning_strategy != annotator_query_strategy):
                         continue
+                    # if (annotator_query_strategy in ['random', 'round-robin'] and
+                    #         learning_strategy != 'majority-vote'):
+                    #     continue
                     label = (f'{instance_query_strategy} '
                              f'+ {annotator_query_strategy} '
-                             f'+ {learning_strategy}'
+                             f'+ {learning_strategy} '
                              f'+ {n_annotator_per_instance}')
-                    df = pd.read_csv(f'{output_path}/result/{label}.csv')
+                    df = pd.read_csv(f'{output_path}/result_letter/{label}.csv')
                     metric_mean = df[f'{metric}_mean'].to_numpy()
                     metric_std = df[f'{metric}_std'].to_numpy()
                     plt.errorbar(np.arange(batch_size, (len(metric_mean) + 1) * batch_size, batch_size), metric_mean,
@@ -60,22 +42,27 @@ def plot_graph(
     plt.tight_layout()
     plt.xlabel('# Labels queried')
     plt.ylabel(f"{metric}")
-    title = f'letter_{metric}'
+    title = f'letter_{metric}_{name}'
     plt.title(title)
-    output_path = f'{output_path}/{title}.pdf'
+    output_path = f'{output_path}/letter_plot/{title}.pdf'
     plt.savefig(output_path, bbox_inches="tight")
 
 
-widgets.interact(
-    plot_graph,
-    is_random=True,
-    is_uncertainty=False,
-    is_core_set=False,
-    is_n_anno_1=False,
-    is_n_anno_2=True,
-    metric=metrics,
+if __name__ == '__main__':
+    metrics = ['misclassification', 'error_annotation_rate']
+    for i in range(10):
+        metrics.append(f"Number_of_annotations_{i}")
+        metrics.append(f"Number_of_correct_annotation_{i}")
+
+    instance_query_strategies = ['random', 'uncertainty', 'coreset']
+    annotator_query_strategies = ['random', 'round-robin', 'trace-reg', 'geo-reg-f', 'geo-reg-w']
+    learning_strategies = ['majority-vote', 'trace-reg', 'geo-reg-f', 'geo-reg-w']
+
+    plot_graph(
+        instance_query_strategies=['random'],
+        annotator_query_strategies=['trace-reg', 'geo-reg-f', 'geo-reg-w'],
+        learning_strategies=learning_strategies,
+        n_annotator_list=[1, 2, 3],
+        metric='misclassification',
+        name='RQ4-intelligent'
     )
-
-
-
-
